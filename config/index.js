9efc38e9f94fc18f813ed322f04822179a7a2d7a
ZhiShou1 <112876381+ZhiShou1@users.noreply.github.com>
2022-09-05 21:49:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "okjFs5gNZDvGAwt53bkvxBSFULPY",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "523fLA5Cd0kHrDX1PaD6batUNF90p26JEAZZtErX5W0",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -31,9 +31,9 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "楠楠",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "okjFs5gNZDvGAwt53bkvxBSFULPYk",
+      id: "okjFs5gNZDvGAwt53bkvxBSFULPY",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "523fLA5Cd0kHrDX1PaD6batUNF90p26JEAZZtErX5W0",
       // 所在省份
